a8a6d66e43fb783fd8b4358c426ec43955dbf477
sukul <sukul.kh@rmuti.ac.th>
2023-01-23 14:00:24 +0700

    6 commit

--- djangoProj138/urls.py
+++ djangoProj138/urls.py
@@ -5,6 +5,7 @@ from ProfileApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('ProfileApp/', include('ProfileApp.urls')),
 
 ]
